2fd7272a21dce827c69eaf260c36c786c82b0633
Muhammad Abdul Jabbaar <muhammadabduljabbaar@gmail.com>
2014-08-18 21:19:51 +0700

    fix typo

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,55 +1,39 @@
 $(function(){
-  var theForm = document.getElementById( 'url-form' );
-  theForm.setAttribute( "autocomplete", "off" );
-
-  new stepsForm( theForm, {
-    onSubmit : function( form ) {
-      classie.addClass( theForm.querySelector( '.simform-inner' ), 'hide' );
-        var $form = $(form),
-          data = $form.serialize();
-        $.post('/', data, function(data){
-          var message = "",
-            message2 = "",
-            notice;
-          if(data.error){
-            for (key in data.message) {
-              if (data.message.hasOwnProperty(key)) {
-                message += data.message[key];
-              }
-            };
-            notice = "error";
-          } else {
-            var to = data.data.to.indexOf('://') > 0 ? data.data.to : 'http://' + data.data.to;
-            message = "Here it comes. Your shortened url";
-            message2 = "Here\'s your shortened url: <a href='"+ to +
-              "'>http://url.to/" + data.data.from + "</a>";
-            notice = "notice";
+  var form = $('#url-form');
+  form.submit(function(e){
+    e.preventDefault();
+    var to = $('#to').val(),
+      data = {to: to};
+      console.log(data);
+      console.log(to);
+    $.post('/', data, function(data){
+      var message = "",
+        notice = "";
+      if(data.error){
+        for (key in data.message) {
+          if (data.message.hasOwnProperty(key)) {
+            message += data.message[key];
           }
-          var notification = new NotificationFx({
-            message : '<span class="icon icon-megaphone"></span><p>'+message+'</p>',
-            layout : 'bar',
-            effect : 'slidetop',
-            type : notice,
-            onClose : function() {
-              // bttn.disabled = false;
-            }
-          });
-          notification.show();
-          var messageEl = document.querySelector( '.final-message' );
-          messageEl.innerHTML = message2;
-          classie.addClass( messageEl, 'show' );
-          var $last = $('.last-urls'),
-            $url = $('p', $last),
-            total = parseInt($('.url-total').html(), 10) || 10,
-            count = $url.length;
-          if (count >= total) {
-            $('p:last-child').hide('slow', $('p:last-child').remove());
-          } else {
-            $('.url-total').html(total + 1);
-          }
-          $('<p><a href="'+data.data.to+'">http://url.to/'+data.data.from+'</a> <small>('+data.data.moment+')</small></p>').prependTo($last);
-        });
-      return false;
-    }
+          
+        };
+        notice = "error";
+      } else {
+        var to = data.data.to.indexOf('://') > 0 ? data.data.to : 'http://' + data.data.to;
+        message = "This is your shortened url: <a href='"+ to +
+          "'>http://url.to/" + data.data.from + "</a>";
+        notice = "notice";
+      }
+      var notification = new NotificationFx({
+				message : '<span class="icon icon-megaphone"></span><p>'+message+'</p>',
+				layout : 'bar',
+				effect : 'slidetop',
+				type : notice,
+				onClose : function() {
+					// bttn.disabled = false;
+				}
+			});
+			notification.show();
+    });
+    return false;
   });
 });
